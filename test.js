2060cc565481a2e506a866ad914ab83aaa2b1173
Michał Wójtowicz <poczta@mwojtowicz.it>
2015-10-26 09:49:19 +0100

    Translator

--- test.js
+++ test.js
@@ -4,10 +4,19 @@ var translator = new Translator('pl_PL', 'en_EN');
 var messages = {
     "Yes" : "Tak",
     "No" : "Nie",
-    "Horses count": "]-Inf,0] Jest %count% koni|{1} Jest %count% koń|[2,5[ Są %count% konie|[5,Inf[ Jest %count% koni"
+    "Horses count": "]-Inf,0] Jest %count% koni|{1} Jest %count% koń|[2,5[ Są %count% konie|[5,Inf[ Jest %count% koni",
+    "Pigs count": [
+        "Jest %count% świnia",
+        "Są %count% świnie",
+        "Jest %count% świni"
+    ]
 };
 
 translator.loadCatalogue(messages);
-var a = translator.transChoice("Horses count", 3);
-var b = translator.trans("Yes");
-console.log(a, b);
+var a = [];
+a.push(translator.pluralize("Horses count", 0));
+a.push(translator.pluralize("Pigs count", 1));
+a.push(translator.pluralize("Pigs count", 2));
+a.push(translator.pluralize("Pigs count", 3));
+a.push(translator.pluralize("Pigs count", 83));
+console.log(a);
--- translator.js
+++ translator.js
@@ -6,14 +6,34 @@ define(function() {
             this.defaultLanguage = defaultLanguage;
             this.fallbackLanguage = fallbackLanguage;
 
+            if(!this.defaultLanguage || !this.fallbackLanguage){
+                throw "You have to set languages";
+            }
+
             this.catalogues = {
                 default: {}
             };
         };
 
+        Translator.prototype._getDefaultLanguage = function(){
+            if(this.defaultLanguage){
+                return this.defaultLanguage;
+            } else if(this.fallbackLanguage){
+                return this.fallbackLanguage;
+            }
+            throw "You have to set languages";
+        };
+
+        Translator.prototype._getFallbackLanguage = function(){
+            if(this.fallbackLanguage){
+                return this.fallbackLanguage;
+            }
+            throw "You have to set languages";
+        };
+
         Translator.prototype.loadCatalogue = function(messages, language, catalogue){
             if(!language){
-                language = this.defaultLanguage;
+                language = this._getDefaultLanguage();
             }
             if(!catalogue){
                 catalogue = 'default';
@@ -28,23 +48,37 @@ define(function() {
             }
         };
 
-        Translator.prototype.setMessage = function(message, catalogue){
+        Translator.prototype.setMessage = function(message, catalogue, language){
             if(!catalogue){
                 catalogue = 'default';
             }
-            this.catalogues[catalogue][message] = message;
+            if(!language){
+                language = this._getDefaultLanguage();
+            }
+            if(!this.catalogues[catalogue][message]){
+                this.catalogues[catalogue][message] = {};
+            }
+            this.catalogues[catalogue][message][language] = message;
         };
 
-        Translator.prototype.deleteMessage = function(message, catalogue){
+        Translator.prototype.deleteMessage = function(message, catalogue, language){
             if(!catalogue){
                 catalogue = 'default';
             }
-            delete this.catalogues[catalogue][message];
+            if(this.catalogues[catalogue] && this.catalogues[catalogue][message]) {
+                if (language) {
+                    if (this.catalogues[catalogue][message][language]) {
+                        delete this.catalogues[catalogue][message][language];
+                    }
+                } else {
+                    delete this.catalogues[catalogue][message];
+                }
+            }
         };
 
         Translator.prototype._getMessage = function(message, catalogue, language){
             if(!language){
-                language = this.defaultLanguage;
+                language = this._getDefaultLanguage();
             }
             if(!catalogue){
                 catalogue = 'default';
@@ -52,12 +86,27 @@ define(function() {
 
             if(this.catalogues[catalogue][message][language]){
                 message = this.catalogues[catalogue][message][language];
-            } else if(this.catalogues[catalogue][message][this.defaultLanguage]){
-                message = this.catalogues[catalogue][message][this.defaultLanguage];
-            } else if(this.catalogues[catalogue][message][this.fallbackLanguage]){
-                message = this.catalogues[catalogue][message][this.fallbackLanguage];
+            } else if(
+                Translator.mainLanguage(language)
+                && this.catalogues[catalogue][message][Translator.mainLanguage(language)]
+            ){
+                message = this.catalogues[catalogue][message][Translator.mainLanguage(language)];
+            } else if(this.catalogues[catalogue][message][this._getDefaultLanguage()]){
+                message = this.catalogues[catalogue][message][this._getDefaultLanguage()];
+            } else if(this.catalogues[catalogue][message][this._getFallbackLanguage()]){
+                message = this.catalogues[catalogue][message][this._getFallbackLanguage()];
             }
             return message;
+        };
+
+        Translator.mainLanguage = function(language){
+            var mainLanguage = language.split('_');
+            if(mainLanguage.length){
+                mainLanguage = mainLanguage[0];
+            } else {
+                mainLanguage = false;
+            }
+            return mainLanguage;
         };
 
         Translator.prototype.trans = function(message, variables, catalogue, language){
@@ -106,7 +155,86 @@ define(function() {
                     }
                 }
             }
-            return message.replace('%count%', variable);
+            return this.replaceMessage(message, variable);
+        };
+
+        Translator.prototype.pluralize = function(message, variable, catalogue, language){
+            var messages = this._getMessage(message, catalogue, language);
+            if(typeof messages === 'string'){
+                return this.transChoice(message, variable, catalogue, language);
+            }
+            if(!language){
+                language = this._getDefaultLanguage();
+            }
+
+            var pluralCondition = Translator.getPluralCondition(language);
+
+            var index = pluralCondition(variable);
+            if(messages[index]){
+                message = messages[index];
+                return this.replaceMessage(message, variable);
+            }
+            throw "There is not "+index+" message in '"+message+"'";
+        };
+
+        Translator.prototype.replaceMessage = function(message, variables){
+            if(
+                Object.prototype.toString.call( variables ) === '[object Array]'
+                || (typeof variables === "object" && !Array.isArray(variables))
+            ) {
+                var regexp;
+                for(var i in variables){
+                    if(variables.hasOwnProperty(i) && i!='length'){
+                        regexp = new RegExp("%"+i+"%");
+                        message = message.replace(regexp, variables[i]);
+                    }
+                }
+            } else {
+                message = message.replace(/%count%/, variables);
+            }
+            return message;
+        };
+
+        Translator.setPluralConditions = function(conditions){
+            for(var i in conditions){
+                if(conditions.hasOwnProperty(i) && i!='length'){
+                    Translator._pluralConditions[i] = conditions[i];
+                }
+            }
+        };
+
+        Translator.setPluralCondition = function(language, condition){
+            Translator._pluralConditions[language] = condition;
+        };
+
+        Translator.deletePluralCondition = function(language){
+            if(language){
+                if(Translator._pluralConditions[language]){
+                    delete Translator._pluralConditions[language];
+                }
+            } else {
+                Translator._pluralConditions = {};
+            }
+        };
+
+        Translator.getPluralCondition = function(language){
+            if(Translator._pluralConditions[language]){
+                return Translator._pluralConditions[language];
+            } else if(Translator._pluralConditions[Translator.mainLanguage(language)]){
+                return Translator._pluralConditions[Translator.mainLanguage(language)];
+            }
+            throw "There is not plural condition for language "+language+" or "+Translator.mainLanguage(language);
+        };
+
+        Translator._pluralConditions = {
+            "en": function(n){return (n != 1);},
+            "de": function(n){return (n != 1);},
+            "fr": function(n){return (n > 1);},
+            "pl": function(n){return (n==1 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);},
+            "ua": function(n){return (n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);},
+            "rm": function(n){ return (n != 1);},
+            "ro": function(n){ return (n==1 ? 0 : (n==0 || (n%100 > 0 && n%100 < 20)) ? 1 : 2);},
+            "ru": function(n){ return (n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);}
         };
 
         return Translator;
